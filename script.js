d3e341e8dc99c92eba9aea5075aa06a8a2a489b1
Mark Conroy <mark@mark.ie>
2022-05-21 18:05:57 +0100

    makes it a _little_ bit prettier

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const allTheseThings = Array.from(localStorage);
-const til = document.querySelector('.til');
-const list = document.createElement('ol');
-const textarea = document.querySelector('.text-for-thing');
-const submit = document.querySelector('.add-item');
-const removeAll = document.querySelector('.remove-all');
-const listItems = list.querySelectorAll('li');
-list.classList.add('things-i-learned');
-til.appendChild(list);
+const itemsLearned = document.querySelector(".items-learned");
+const list = document.createElement("ol");
+const textarea = document.querySelector(".text-for-thing");
+const submit = document.querySelector(".add-item");
+const removeAll = document.querySelector(".remove-all");
+const listItems = list.querySelectorAll("li");
+list.classList.add("things-i-learned");
+itemsLearned.appendChild(list);
 
 for (let i = 1; i < localStorage.length + 1; i++) {
-  const element = document.createElement('li');
-  element.classList.add('thing-i-learned');
+  const element = document.createElement("li");
+  element.classList.add("thing-i-learned");
   list.appendChild(element);
   element.innerHTML = localStorage[i];
   itemKey = localStorage.length + 1;
 }
 
-submit.addEventListener('click', function() {
+submit.addEventListener("click", function () {
   const text = textarea.value;
-  const element = document.createElement('li');
-  element.classList.add('thing-i-learned'); 
-  element.innerHTML = text;
-  list.appendChild(element);
-  localStorage.setItem(localStorage.length + 1, text);
+  if (text) {
+    const element = document.createElement("li");
+    element.classList.add("thing-i-learned");
+    element.innerHTML = text;
+    list.appendChild(element);
+    localStorage.setItem(localStorage.length + 1, text);
+  }
 });
 
-removeAll.addEventListener('click', function() {
+removeAll.addEventListener("click", function () {
   localStorage.clear();
-  list.querySelectorAll('li').forEach(item => {
+  list.querySelectorAll("li").forEach((item) => {
     item.remove();
-  })
-})
-
-
+  });
+});
