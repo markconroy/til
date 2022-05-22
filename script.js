12cb5ca0c14d05aad00dd88910221fff2351710c
Mark Conroy <mark@mark.ie>
2022-05-22 11:56:16 +0100

    adds very basic social sharing

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ const listItems = list.querySelectorAll("li");
 list.classList.add("things-i-learned");
 itemsLearned.appendChild(list);
 
+https://twitter.com/share?url=https%3A//www.annertech.com/node/254&text=Allowing%20Editors%20to%20Choose%20the%20Image%20Style%20in%20Drupal%208%20Image%20Fields
+
 for (let i = 1; i < localStorage.length + 1; i++) {
   const element = document.createElement("li");
   element.classList.add("thing-i-learned");
   list.appendChild(element);
-  element.innerHTML = localStorage[i];
+  element.innerHTML = `
+    ${localStorage[i]}
+    <ul class="share-list">
+      <li class="share-list__item">Share on <a class="share-list__link" href="https://twitter.com/share?url=https://til.mark.ie&text=Today I learned: ${localStorage[i]} - shared via til app" target="_blank">Twitter</a></li>
+      <li class="share-list__item">Share on <a class="share-list__link" href="https://www.facebook.com/sharer.php?u=https://til.mark.ie&quote=Today I learned: ${localStorage[i]} - shared via til app" target="_blank">Facebook</a></li>
+    </ul>
+  `;
   itemKey = localStorage.length + 1;
 }
 
@@ -20,7 +28,13 @@ submit.addEventListener("click", function () {
   if (text) {
     const element = document.createElement("li");
     element.classList.add("thing-i-learned");
-    element.innerHTML = text;
+    element.innerHTML = `
+      ${text}
+      <ul class="share-list">
+        <li class="share-list__item">Share on <a class="share-list__link" href="https://twitter.com/share?url=https://til.mark.ie&text=Today I learned: ${text} - shared via til app" target="_blank">Twitter</a></li>
+        <li class="share-list__item">Share on <a class="share-list__link" href="https://www.facebook.com/sharer.php?u=https://til.mark.ie&quote=Today I learned: ${text} - shared via til app" target="_blank">Facebook</a></li>
+      </ul>
+    `;
     list.appendChild(element);
     localStorage.setItem(localStorage.length + 1, text);
     textarea.value = '';
