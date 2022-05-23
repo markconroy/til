c49430da8373de927dcb5448e5368914678999d8
Mark Conroy <mark@mark.ie>
2022-05-23 09:57:14 +0100

    reverse order of learned items

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const list = document.createElement("ol");
 const textarea = document.querySelector(".thing__thing");
 const submit = document.querySelector(".add-item");
 const removeAll = document.querySelector(".remove-all");
-const listItems = list.querySelectorAll("li");
 list.classList.add("things-i-learned");
 itemsLearned.appendChild(list);
 
-https://twitter.com/share?url=https%3A//www.annertech.com/node/254&text=Allowing%20Editors%20to%20Choose%20the%20Image%20Style%20in%20Drupal%208%20Image%20Fields
-
-for (let i = 1; i < localStorage.length + 1; i++) {
+for (let i = localStorage.length; i >= 1; i--) {
   const element = document.createElement("li");
   element.classList.add("thing-i-learned");
   list.appendChild(element);
@@ -25,6 +22,7 @@ for (let i = 1; i < localStorage.length + 1; i++) {
     </ul>
   `;
   itemKey = localStorage.length + 1;
+
 }
 
 submit.addEventListener("click", function () {
@@ -43,7 +41,7 @@ submit.addEventListener("click", function () {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/></svg></a></li>
       </ul>
     `;
-    list.appendChild(element);
+    list.insertBefore(element, document.querySelector('.thing-i-learned'));
     localStorage.setItem(localStorage.length + 1, text);
     textarea.value = '';
     textarea.placeholder = 'Anything else?';
